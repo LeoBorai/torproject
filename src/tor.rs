724a273ae211111ed541cd1c105891dc4113bc72
Esteban Borai <estebanborai@gmail.com>
2025-01-20 23:18:20 -0300

    feat: run tor and keep pid

--- src/tor.rs
+++ src/tor.rs
@@ -1,11 +1,17 @@
 use std::path::PathBuf;
-use std::process::{Child, Command};
+use std::process::Stdio;
 
-use anyhow::{Context, Result};
+use anyhow::{Context, Error, Result};
+use tokio::io::{AsyncBufReadExt, BufReader};
+use tokio::process::Command;
 
 use super::{Downloader, DOWNLOAD_DIRECTORY_TOR};
 
+/// Message printed on Tor Console when completely bootstraped.
+const TOR_BOOTSTRAPED_LOG: &str = "Bootstrapped 100% (done): Done";
+
 pub struct Tor {
+    pid: Option<u32>,
     path: PathBuf,
     version: String,
 }
@@ -15,12 +21,18 @@ impl Tor {
     /// of [`Tor`] to interact with Expert Bundle binaries.
     pub async fn setup() -> Result<Tor> {
         let downloader = Downloader::default();
-        downloader.download().await?;
+        // downloader.download().await?;
 
         Ok(Tor {
+            pid: None,
             path: downloader.download_dir_path(),
             version: downloader.version().to_owned(),
         })
+    }
+
+    #[inline]
+    pub fn pid(&self) -> Option<u32> {
+        self.pid
     }
 
     #[inline]
@@ -28,20 +40,52 @@ impl Tor {
         &self.version
     }
 
-    pub fn run(&self) -> Result<Child> {
+    pub async fn run(&mut self) -> Result<u32> {
         // https://stackoverflow.com/questions/34611742/how-do-i-read-the-output-of-a-child-process-without-blocking-in-rust
         let bin_path = self.tor_bin_dir_path();
         let tor_bin = bin_path.join("tor");
-        let child = Command::new(tor_bin)
+        let mut child = Command::new(tor_bin)
+            .stdout(Stdio::piped())
             .spawn()
             .context("Failed to spawn Tor Process")?;
+        let pid = child.id().ok_or(Error::msg("No Process ID for Tor"))?;
 
-        Ok(child)
+        self.pid = Some(pid);
+
+        let stdout = child.stdout.take().context("Failed to retrieve Stdout")?;
+        let mut reader = BufReader::new(stdout).lines();
+
+        tokio::spawn(async move {
+            let status = child.wait().await.expect("Tor Process errored.");
+        });
+
+        while let Some(line) = reader.next_line().await? {
+            if line.contains(TOR_BOOTSTRAPED_LOG) {
+                break;
+            }
+        }
+
+        Ok(pid)
+    }
+
+    pub fn kill(&mut self) -> Result<()> {
+        if let Some(pid) = &mut self.pid {
+            return Ok(());
+        }
+
+        anyhow::bail!("No process for Tor avaialable.")
     }
 
     fn tor_bin_dir_path(&self) -> PathBuf {
         let dl_path = self.path.clone();
         dl_path.join(DOWNLOAD_DIRECTORY_TOR)
+    }
+}
+
+impl Drop for Tor {
+    fn drop(&mut self) {
+        // intentionally ignore error due to exec context
+        let _ = self.kill();
     }
 }
 
@@ -55,6 +99,15 @@ mod tests {
         let version = tor.version();
 
         assert_eq!(version, DEFAULT_VERSION);
+    }
+
+    #[tokio::test]
+    async fn pid_returned_by_run_matches() {
+        let mut tor = Tor::setup().await.expect("Failed to setup a Tor instance.");
+        let tor_pid = tor.run().await.expect("Failed to run Tor Proxy");
+        let instance_pid = tor.pid().unwrap();
+
+        assert_eq!(tor_pid, instance_pid);
     }
 
     // #[tokio::test]
