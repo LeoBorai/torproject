ff01ea01b3f450a97d1ad36ae6e91a2441f46dd4
Leo Borai <estebanborai@gmail.com>
2025-03-31 22:12:40 -0300

    chore: test to check conn established

--- src/lib.rs
+++ src/lib.rs
@@ -20,3 +20,28 @@ impl Default for VersionSelection {
         Self::Version(DEFAULT_VERSION.to_string())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use anyhow::Result;
+    use reqwest::{Client, Proxy};
+
+    use crate::Tor;
+
+    const TOR_CHECK_WEB: &str = "https://check.torproject.org/";
+    const TOR_SOCK5_LOCAL: &str = "socks5://127.0.0.1:9050";
+
+    #[tokio::test]
+    async fn check_tor_conn() -> Result<()> {
+        let mut tor = Tor::setup().await?;
+        tor.run().await?;
+        let proxy = Proxy::all(TOR_SOCK5_LOCAL)?;
+        let client = Client::builder().proxy(proxy).build()?;
+        let res_html = client.get(TOR_CHECK_WEB).send().await?.text().await?;
+        let contains = res_html.contains("Congratulations. This browser is configured to use Tor.");
+
+        assert!(contains);
+
+        Ok(())
+    }
+}
